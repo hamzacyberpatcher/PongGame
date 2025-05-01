f79d16a5e3b7583e34a2586255cc50ead4666f23
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-05-01 13:20:16 +0500

    setting up sfml

--- PongGame/main.cpp
+++ PongGame/main.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
 using namespace std;
+
+const int WIDTH = 1600;
+const int HEIGHT = 900;
 
 int main()
 {
-	cout << "hello world\n";
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Comway's Game of Life", sf::Style::Default);
+	window.setFramerateLimit(60);
+
+	sf::RectangleShape rectangle;
+	rectangle.setSize(sf::Vector2f(100, 50));
+	rectangle.setOutlineColor(sf::Color::Red);
+	rectangle.setOutlineThickness(5);
+	rectangle.setPosition({ 10, 20 });
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+
+		window.draw(rectangle);
+
+		window.display();
+	}
+
+	return 0;
+
 	return 0;
 }
