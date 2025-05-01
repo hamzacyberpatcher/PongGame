6c6b40ffc23017962039c32fe3533a243a5f5fd8
hamzacyberpatcher <hamzaoffuture@gmail.com>
2025-05-01 13:50:10 +0500

    added all of the core game element classes

--- PongGame/main.cpp
+++ PongGame/main.cpp
@@ -56,6 +56,48 @@ public:
 	Vector2d(float x = 0, float y = 0) : x(x), y(y) {}
 };
 
+// all of the core game elements
+
+class Ball
+{
+	Vector2d m_position;
+	float m_radius;
+	Vector2d m_velocity;
+public:
+	Ball(Vector2d startingPosition, float radius)
+	{
+		m_position = startingPosition;
+		m_radius = radius; 
+	}
+
+	Vector2d getPositon() { return m_position; }
+
+	float getRadius() { return m_radius; }
+
+	Vector2d getVelocity() { return m_velocity; }
+
+	void setPosition(Vector2d position) { m_position = position; }
+
+	void setVelocity(Vector2d velocity) { m_velocity = velocity; }
+};
+
+class Paddle
+{
+	RectangleShape m_rect;
+public:
+	Paddle(RectangleShape startingPosition) { m_rect = startingPosition; }
+	RectangleShape GetPositionSize() { return m_rect; }
+	void SetPositionSize(RectangleShape newPositionAndSize) { m_rect = newPositionAndSize; }
+};
+
+class Court
+{
+	RectangleShape m_dimensions;
+public:
+	Court(RectangleShape dimensions) : m_dimensions(dimensions) {}
+	RectangleShape GetDimensions() { return m_dimensions; }
+};
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Comway's Game of Life", sf::Style::Default);
